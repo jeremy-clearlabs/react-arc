e83a5704445a3b7524156823bff8fa018a4be29a
Werther <werther.dong@clearlabs.com>
2018-03-13 12:45:02 -0700

    seems like Button extended from react-router-dom link.

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-import { Heading } from 'components'
+import { Heading, Button } from 'components'
 
 const Wrapper = styled.div`
   width: 100%;
@@ -14,7 +14,7 @@ const HomePage = () => {
   return (
     <Wrapper>
       <Heading level={1}>Clear Labs</Heading>
-      <Link to="/login">Login</Link>
+      <Button to="/login">Login</Button>
     </Wrapper>
   )
 }
