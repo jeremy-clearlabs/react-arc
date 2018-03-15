84f1d7d21c4bd2879c273410cb19dfe63f6815f1
jermspeaks <jermspeaks@gmail.com>
2018-03-15 10:08:37 -0700

    practice enzyme testing

--- src/components/molecules/Alert/index.test.js
+++ src/components/molecules/Alert/index.test.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import Alert from '.'
 
-const wrap = (props = {}) => shallow(<Alert {...props} />)
+const onClose = jest.fn()
+
+const wrap = (props = {}) => shallow(<Alert onClose={onClose} {...props} />)
 
 it('renders children when passed in', () => {
   const wrapper = wrap({ children: 'test' })
@@ -12,4 +14,38 @@ it('renders children when passed in', () => {
 it('renders props when passed in', () => {
   const wrapper = wrap({ id: 'foo' })
   expect(wrapper.find({ id: 'foo' })).toHaveLength(1)
+})
+
+it('renders variant', () => {
+  const wrapper = wrap({ variant: 'primary' })
+  expect(wrapper.find({ variant: 'primary' })).toHaveLength(1)
+})
+
+it('renders block within Child', () => {
+  const wrapper = wrap()
+  expect(wrapper.children()).toHaveLength(1)
+})
+
+it('does not render the close icon when not passed dismissable', () => {
+  const wrapper = wrap()
+  expect(wrapper.find('IconButton')).toHaveLength(0)
+})
+
+it('renders close icon when passed dismissable', () => {
+  const wrapper = wrap({ dismissable: true })
+  expect(wrapper.find('IconButton')).toHaveLength(1)
+})
+
+it('renders nothing when close icon is clicked', () => {
+  const wrapper = wrap({ dismissable: true })
+  const close = wrapper.find('IconButton')
+  expect(wrapper.state('isOpen')).toBe(true)
+  close.simulate('click')
+  expect(wrapper.state('isOpen')).toBe(false)
+})
+
+it('mounts with different combination of props', () => {
+  mount(<Alert dismissable />)
+  // mount(<Alert dismissable />)
+  // mount(<Alert dismissable />)
 })
